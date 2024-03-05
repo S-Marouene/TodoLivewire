511cf985d660d524fd750baa5f42da5c3915d2db
saidi marouene <saidi.marouen@gmail.com>
2024-03-05 09:48:29 +0100

    vite web sync

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo App template</title>
     <script src="https://cdn.tailwindcss.com"></script>
-
+    @vite('resources/css/app.js')
 </head>
 <body>
-
-
 
 <livewire:navbar/>
 
